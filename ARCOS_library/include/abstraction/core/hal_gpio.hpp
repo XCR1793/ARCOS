343175daa20aed4801610486c6b8e1108267dcaf
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-09-07 03:34:13 +1000

    Added Hal class requirements

--- ARCOS_library/include/abstraction/core/hal_gpio.hpp
+++ ARCOS_library/include/abstraction/core/hal_gpio.hpp
@@ -1,37 +1,94 @@
-/**
+/*****************************************************************
  * File:      hal_gpio.hpp
  * Category:  abstraction
  * Author:    XCR1793 (Feather Forge)
  * 
  * Purpose:
  *    Provides a template for GPIO HAL in both single and parallel
- *    operations of digital pins.
- */
+ *    operations of digital pins. Compile time HAL abstraction for
+ *    a single port with zero runtime overhead.
+ *****************************************************************/
 
 #ifndef ARCOS_ABSTRACTION_CORE_HAL_GPIO_HPP_
 #define ARCOS_ABSTRACTION_CORE_HAL_GPIO_HPP_
 
-namespace arcos::hal::gpio{
-  enum struct GpioHalPinMode{
-    Input = 0,
-    Output = 1
-  };
+namespace arcos::abstraction{
+  namespace gpio{
+    /** Set GPIO pins as Inputs or Outputs */
+    enum struct GpioHalPinMode{
+      Input = 0,
+      Output = 1
+    };
 
-  enum struct GpioHalPinPull{
-    Float = 0,
-    PullUp = 1,
-    PullDown = 2
-  };
+    /** Set GPIO pins internal pull up or downs */
+    enum struct GpioHalPinPull{
+      Float = 0,
+      PullUp = 1,
+      PullDown = 2
+    };
 
-  enum struct GpioHalPortMode{
-    Single = 0,
-    Parallel = 1
-  };
+    /** Set GPIO pin buses (port) as single or parallel driven */
+    enum struct GpioHalPortMode{
+      Single = 0,
+      Parallel = 1
+    };
 
-  struct GpioHalPinAddress{
-    void* port;
-    uint32_t mask; // Can be recast if need be
+    /** Structure for storing the GPIO pin buses */
+    template <typename PinBankType>
+    struct GpioHalPinAddress{
+      void* port;
+      PinBankType pinbank; // Can be recast if need be
+    };
+    /** GPIO port sizes */
+    using GpioHalPinAddress8  = GpioHalPinAddress<uint8_t>;
+    using GpioHalPinAddress16 = GpioHalPinAddress<uint16_t>;
+    using GpioHalPinAddress32 = GpioHalPinAddress<uint32_t>;
+
+  } // arcos hardware abstraction layer namespace specifically gpio
+
+  template <typename  PlatformImplentation,
+                      gpio::GpioHalPortMode PortMode,
+                      gpio::GpioHalPinMode PinMode>
+  class HalGpio{
+    public:
+      constexpr HalGpio() noexcept = default;
+      ~HalGpio() = default;
+
+      /**
+       * @brief Initialises GPIO Pins & Bus
+       */
+      static void Init(PlatformImplentation& impl){
+        impl.hal_gpio_initialisation();
+      };
+      
+      /**
+       * @brief Sets Pins as input or output
+       */
+
+      /**
+       * @brief Sets Pull direction for pins
+       */
+
+      /**
+       * @brief Sets Port mode (single or parallel)
+       */
+
+      /**
+       * @brief Writes a pin as high or low
+       */
+
+      /**
+       * @brief Writes a set of pins in parallel
+       */
+
+      /**
+       * @brief Reads a pin as high or low
+       */
+
+      /**
+       * @brief Reads a set of pins in parallel
+       */
   };
-} // arcos hardware abstraction layer namespace specifically gpio
+}
 
 #endif // ARCOS_ABSTRACTION_CORE_HAL_GPIO_HPP_
